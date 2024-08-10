8793c76b6e839928fe8e4de8c6cf08bf7a30a63a
Walid Tolba <wrt0624@gmail.com>
2024-08-10 19:05:11 +0100

    Fix all animation

--- lib/widgets/swipable_text_widget.dart
+++ lib/widgets/swipable_text_widget.dart
@@ -22,26 +22,49 @@ class _SwipableTextWidgetState extends State<SwipableTextWidget> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        SwipeTextWidget(direction: 'left', callback: () {
+        SwipeTextWidget(
+            direction: 'left',
+            callback: () {
               swipDirection = 'left';
               widget.leftCallback();
             }),
-        AnimatedBuilder(
-          animation: widget.controller,
-          builder: (context, child) {
-            double value = widget.controller.value;
-            return Opacity(
-              opacity: 1 - value,
-              child: Transform.translate(
-                  offset: Offset(
-                      (swipDirection == 'left' ? -1 : 1) * value * 250, 0),
-                  child: child),
-            );
-          },
-          child: Text(
-            '2024-01-01 - 2024-01-08',
-            style: TextStyle(color: Color(0xFFD6BD98), fontSize: 16.0),
-          ),
+        Stack(
+          children: [
+            AnimatedBuilder(
+              animation: widget.controller,
+              builder: (context, child) {
+                double value = widget.controller.value;
+                return Opacity(
+                  opacity: 1 - value,
+                  child: Transform.translate(
+                      offset: Offset(
+                          (swipDirection == 'left' ? -1 : 1) * value * 250, 0),
+                      child: child),
+                );
+              },
+              child: Text(
+                '2024-01-01 - 2024-01-08',
+                style: TextStyle(color: Color(0xFFD6BD98), fontSize: 16.0),
+              ),
+            ),
+            AnimatedBuilder(
+              animation: widget.controller,
+              builder: (context, child) {
+                double value = widget.controller.value;
+                int directionFactor = (swipDirection == 'left' ? -1 : 1);
+                return Opacity(
+                  opacity: value,
+                  child: Transform.translate(
+                      offset: Offset(directionFactor * 250 * (value - 1), 0),
+                      child: child),
+                );
+              },
+              child: Text(
+                '2024-01-01 - 2024-01-08',
+                style: TextStyle(color: Color(0xFFD6BD98), fontSize: 16.0),
+              ),
+            ),
+          ],
         ),
         SwipeTextWidget(
             direction: 'right',
